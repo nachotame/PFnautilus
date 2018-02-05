035fbabcacbf8903cc0a9467ceb8b59ded5b726f
nachotame <35261491+nachotame@users.noreply.github.com>
2018-02-05 13:31:27 +0100

    Realizado la llamada de api

--- app/src/main/java/com/example/nacho/pfnautilus/MainActivity.java
+++ app/src/main/java/com/example/nacho/pfnautilus/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nacho.pfnautilus;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mtextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mtextview=(TextView) findViewById(R.id.mtextview);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +41,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void apiLluvia (View v){
+        //instanciamos la respuesta queue
+        RequestQueue llamada = Volley.newRequestQueue(this);
+        String url="api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=1a3a9ef7c45a8d64f5f26a847eaa2734\n";
+
+        //solicitud de llamada de respuesta de URL
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject mainObject = new JSONObject(response);
+                            mtextview.setText(mainObject.getString("city"));
+                            //tienes que poner que TIPO de objeto va a recoger si no dará error
+
+                        } catch (JSONException jsone) {
+                            jsone.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mtextview.setText(error.getMessage());
+
+            }
+        });
+    //añade solicitud cola de respuesta
+        llamada.add(stringRequest);
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
